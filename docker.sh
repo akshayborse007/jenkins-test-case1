7fa3dcde6fe55f1a076c6fe4541d3fec9ec20ac2
ab <ab@mail>
2023-01-27 09:44:25 +0530

    added

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
-pwd
-sudo apt update
-sudo apt install openjdk-11-jdk -y
-sudo apt install docker.io -y
-#sadsdaafff
+# pwd
+# sudo apt update
+# sudo apt install openjdk-11-jdk -y
+# sudo apt install docker.io -y
+# #sadsdaafff
