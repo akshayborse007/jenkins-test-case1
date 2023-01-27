8d2048e3d63624fdfe4bf33ca290800906827393
ab <ab@mail>
2023-01-27 08:23:49 +0530

    done

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,12 @@
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
-sudo apt-get install jenkins
+sudo apt update
+sudo apt install openjdk-11-jdk -y
+sudo apt install docker.io -y
+docker run -itd --name jenkins -p 80:80 jenkins/jenkins
+
+# curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+#   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+# echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+#   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+#   /etc/apt/sources.list.d/jenkins.list > /dev/null
+# sudo apt-get update
+# sudo apt-get install jenkins
